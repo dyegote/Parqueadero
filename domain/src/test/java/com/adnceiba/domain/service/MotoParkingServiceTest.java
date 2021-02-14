cce2fea7af9bcbe81aeb003a0bac10859e4c93ce
diego.tobar <dyegote@hotmail.com>
2021-02-14 15:09:39 -0500

    Code Smell

--- domain/src/test/java/com/adnceiba/domain/service/MotoParkingServiceTest.java
+++ domain/src/test/java/com/adnceiba/domain/service/MotoParkingServiceTest.java
@@ -19,6 +19,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static org.junit.Assert.assertEquals;
+
 public class MotoParkingServiceTest {
 
     private MotoParkingService motoParkingService;
@@ -94,7 +96,7 @@ public class MotoParkingServiceTest {
     }
 
     @Test
-    public void enterVehicle_motoNotExist_allowEnter() {
+    public void enterVehicle_parkingNotExist_allowEnter() {
         //Arrange
         Moto moto = new MotoBuilder().withLicensePlate("BXC123").build();
         Parking parking = new ParkingBuilder(moto).build();
@@ -107,6 +109,56 @@ public class MotoParkingServiceTest {
         //Assert
         Mockito.verify(motoRepository).save(moto);
         Mockito.verify(parkingRepository).save(parking);
+    }
+
+    @Test(expected = EntryNotAllowedException.class)
+    public void enterVehicle_parkingExist_allowEnter() {
+        //Arrange
+        Moto moto = new MotoBuilder().withLicensePlate("BXC123").build();
+        Parking parking = new ParkingBuilder(moto).build();
+        Mockito.when(parkingRepository.getAmountMoto()).thenReturn(5);
+        Mockito.when(parkingRepository.getByLicensePlate("BXC123")).thenReturn(parking);
+
+        //Act
+        motoParkingService.enterVehicle(parking);
+
+        //Assert
+        Mockito.verify(motoRepository).save(moto);
+        Mockito.verify(parkingRepository).save(parking);
+    }
+
+    @Test
+    public void calculatePrice_withCylinderMoto_correctPrice() throws Exception {
+        //Arrange
+        Date star = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2021-01-09 08:00:00");
+        Date end = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2021-01-11 11:00:00");
+        Parking parking = new ParkingBuilder(new MotoBuilder().withCylinder(600).build())
+                .withArrivingTime(star)
+                .withLeavingTime(end).build();
+        MotoParkingService parkingService = new MotoParkingService();
+
+        //Act
+        float total = parkingService.calculatePrice(parking);
+
+        //Assert
+        assertEquals(11500, total, 0.05);
+    }
+
+    @Test
+    public void calculatePrice_withOutCylinderMoto_correctPrice() throws Exception {
+        //Arrange
+        Date star = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2021-01-09 08:00:00");
+        Date end = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2021-01-11 11:00:00");
+        Parking parking = new ParkingBuilder(new MotoBuilder().withCylinder(200).build())
+                .withArrivingTime(star)
+                .withLeavingTime(end).build();
+        MotoParkingService parkingService = new MotoParkingService();
+
+        //Act
+        float total = parkingService.calculatePrice(parking);
+
+        //Assert
+        assertEquals(9500, total, 0.05);
     }
 
 }
--- domain/src/test/java/com/adnceiba/domain/service/ParkingServiceTest.java
+++ domain/src/test/java/com/adnceiba/domain/service/ParkingServiceTest.java
@@ -31,21 +31,6 @@ public class ParkingServiceTest {
         assertEquals(19000, total, 0.05);
     }
 
-    @Test
-    public void calculatePrice_withCylinderMoto_correctPrice() throws Exception {
-        //Arrange
-        Date star = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2021-01-09 08:00:00");
-        Date end = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse("2021-01-11 11:00:00");
-        Parking parking = new ParkingBuilder(new MotoBuilder().withCylinder(600).build())
-                .withArrivingTime(star)
-                .withLeavingTime(end).build();
-        MotoParkingService parkingService = new MotoParkingService();
 
-        //Act
-        float total = parkingService.calculatePrice(parking);
-
-        //Assert
-        assertEquals(11500, total, 0.05);
-    }
 
 }
