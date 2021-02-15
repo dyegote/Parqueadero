c22f7adefa94dc874db31930c29ea54a0a541beb
diego.tobar <dyegote@hotmail.com>
2021-02-14 22:18:12 -0500

    License plate exception

--- domain/src/main/java/com/adnceiba/domain/entity/Vehicle.java
+++ domain/src/main/java/com/adnceiba/domain/entity/Vehicle.java
@@ -1,14 +1,28 @@
 package com.adnceiba.domain.entity;
 
+import com.adnceiba.domain.exception.DomainException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public abstract class Vehicle {
 
+    private static final String INVALID_LICENSE_PLATE = "Invalid license plate.";
     private String licensePlate;
 
     public Vehicle(String licensePlate) {
-       this.licensePlate = licensePlate;
+       this.setLicensePlate(licensePlate);
     }
 
     public String getLicensePlate() {
         return licensePlate;
+    }
+
+    private void setLicensePlate(String licensePlate) {
+        Matcher m = Pattern.compile("[A-Z][A-Z]([A-Z]|\\d)\\d\\d").matcher(licensePlate);
+        if (!m.find()) {
+            throw new DomainException(INVALID_LICENSE_PLATE);
+        }
+        this.licensePlate = licensePlate;
     }
 }
